f81244f3463958136de96d08d3c3d40a5e6c0c6b
Thomas Edwards <thce28@gmail.com>
2019-02-14 18:28:00 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,11 @@
+require 'pry'
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    arr = []
+    # DONT NEED TO PASS IN HASH
+    x = .key(arguments)
+    
+    binding.pry 
+  # returns an array with every key from hash == arguement value 
   end
 end
